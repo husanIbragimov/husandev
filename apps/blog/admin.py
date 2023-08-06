f4ad763c78122d108f1dd61d902e68ab4786868a
husanibragimov <ibragimovdev@tstu.uz>
2023-08-06 23:50:45 +0500

    create admin panel

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class BlogImagesInline(admin.StackedInline):
+    model = BlogImages
+    extra = 1
+
+
+@admin.register(BlogNews)
+class BlogNewsAdmin(admin.ModelAdmin):
+    inlines = [BlogImagesInline]
+    list_display = ('title', 'created_at', 'id')
+
+
+class ExperienceInline(admin.TabularInline):
+    model = Experience
+    extra = 1
+    readonly_fields = ('created_at',)
+
+
+class EducationInline(admin.TabularInline):
+    model = Education
+    extra = 1
+    readonly_fields = ('created_at',)
+
+
+class MeImagesInline(admin.TabularInline):
+    model = MeImages
+    extra = 1
+
+
+@admin.register(AboutMe)
+class AboutMeAdmin(admin.ModelAdmin):
+    inlines = [MeImagesInline, ExperienceInline, EducationInline]
+    list_display = ('title', 'age')
+    # readonly_fields = ('my_age',)
+
+
+admin.site.register(Resume)
+admin.site.register(MeImages)
+admin.site.register(Education)
+admin.site.register(Experience)
