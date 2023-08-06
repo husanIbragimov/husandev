68d85a8d8b25101d481a78d7d5819b509138aca6
husanibragimov <ibragimovdev@tstu.uz>
2023-08-06 23:44:52 +0500

    update model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from datetime import datetime
 
 
 class Resume(models.Model):
-    resume = models.ImageField(upload_to='media/resume/', null=True)
+    resume = models.FileField(upload_to='media/resume/', null=True)
     created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -30,14 +31,51 @@ class BlogImages(models.Model):
 
 class AboutMe(models.Model):
     title = models.CharField(max_length=225, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=225, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=17, null=True, blank=True)
+    degree = models.CharField(max_length=15, null=True, blank=True)
+    interest = models.CharField(max_length=50, null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
+
+    @property
+    def get_age(self):
+        current_age = datetime.now().date().year - self.birthday.year
+        print(current_age)
+        self.age = current_age
+        self.save()
+        return current_age
 
     def __str__(self):
         return f"{self.title}"
 
 
+class Experience(models.Model):
+    blog = models.ForeignKey(AboutMe, on_delete=models.CASCADE, null=True, blank=True, related_name='experiences')
+    experience = models.CharField(max_length=50, null=True, blank=True)
+    position = models.CharField(max_length=31, null=True, blank=True)
+    period = models.CharField(max_length=47, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.experience} - {self.period}"
+
+
+class Education(models.Model):
+    blog = models.ForeignKey(AboutMe, on_delete=models.CASCADE, null=True, blank=True, related_name='educations')
+    education = models.CharField(max_length=50, null=True, blank=True)
+    degree = models.CharField(max_length=15, null=True, blank=True)
+    period = models.CharField(max_length=15)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.education} - {self.period}"
+
+
 class MeImages(models.Model):
-    blog = models.ForeignKey(AboutMe, on_delete=models.CASCADE, null=True, blank=True)
+    blog = models.ForeignKey(AboutMe, on_delete=models.CASCADE, null=True, blank=True, related_name='my_images')
     image = models.ImageField(upload_to=f'media/about_me/', null=True, blank=True)
     description = RichTextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
