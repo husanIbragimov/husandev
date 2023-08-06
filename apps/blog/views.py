aefc2b2aa6589adf81c1b7a5c6a4b767ed5d663a
husanibragimov <ibragimovdev@tstu.uz>
2023-08-06 23:52:51 +0500

    update views

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import BlogNews, AboutMe, Education, Experience, MeImages, Resume
+from apps.service.models import BannerDetail
 
-# Create your views here.
+
+def index(request):
+    # main page
+    about_qs = AboutMe.objects.all().last()
+    educations = Education.objects.all().order_by("-id")
+    experiences = Experience.objects.all().order_by("-id")
+    my_images = MeImages.objects.filter(blog_id=about_qs).order_by('-id')
+    resume = Resume.objects.last()
+    ctx = {
+        "resume": resume,
+        "about": about_qs,
+        "about_imgs": my_images,
+        "educations": educations,
+        "experiences": experiences
+    }
+    return render(request, 'index.html', ctx)
